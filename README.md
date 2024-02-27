c5b3484176dbf475354237882131cfe28c2ee182
Fabian Wagner <wafab112@gmail.com>
2024-02-27 10:45:06 +0100

    add some docs

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # HTTP-test-maker
 A cli-tool for creating scripts, 
-that execute various a http-endpoint after authenticating.
+that execute various REST-endpoints (using the JSON-file-format) after authenticating.
 
 ## How does it work
-
+When using a unix-like system, you can just call
+```bash
+$ ./http-tm.py -h
+```
+to see helpful information. It will call the python script via bash 
+(provided it finds a `python3` interpreter in your `$PATH`).
+If you are using windows or you do not have a `python3` in your `$PATH`, you must either change the shebang
+in `http-tm.py` or you call the regular way:
+```bash
+$ /path/to/python http-tm.py -h
+```
--- cli.py
+++ cli.py
@@ -3,6 +3,11 @@ from jproperties import Properties
 from handlers.config_handler import ConfigHandler
 
 class Cli:
+    """
+    Creates the cli arguments and parses them.
+    Takes the arguments and notifies the specific handlers to handle the arguments.
+    """
+
     def __init__(self):
         parser = argparse.ArgumentParser(
             prog = "http-tm.py", 
@@ -16,14 +21,14 @@ class Cli:
             action="store_true", 
             help="USE WITH CAUTION: Assumes yes whenever a interactive yes/no question is asked. Is applied to subcommands.")
 
-        subparsers = parser.add_subparsers(dest="command")
+        commandParsers = parser.add_subparsers(dest="command")
 
-        config_parser = subparsers.add_parser("config", description="change the user's config of default values", help="change config, see %(prog)s config -h")
-        config_parser.add_argument("-l", "--list", action="store_true", help="lists all current configs")
-        config_parser.add_argument("--get-location", action="store_true", help="prints the location of the config file")
-        config_parser.add_argument("-u", "--user", help="new default username")
-        config_parser.add_argument("-p", "--pass", dest="password", metavar="pass", help="new default password")
-        config_parser.add_argument("--recreate", help="creates a new config file", action="store_true")
+        configParser = commandParsers.add_parser("config", description="change the user's config of default values", help="change config, see %(prog)s config -h")
+        configParser.add_argument("-l", "--list", action="store_true", help="lists all current configs")
+        configParser.add_argument("--get-location", action="store_true", help="prints the location of the config file")
+        configParser.add_argument("-u", "--user", help="new default username")
+        configParser.add_argument("-p", "--pass", dest="password", metavar="pass", help="new default password")
+        configParser.add_argument("--recreate", help="creates a new config file", action="store_true")
         
         args = parser.parse_args()
 
